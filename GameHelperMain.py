432e4bba5e27ed73dbd02609b7c5113d1da1417d
Zac Patel <zpatel@berkeley.edu>
2017-02-13 20:25:12 -0800

    Started working on refactoring WordHelper out of the program.

--- GameHelperMain.py
+++ GameHelperMain.py
@@ -1,7 +1,7 @@
 # Note, remove the shebang before uploading to AWS
 """
 RPG Helper Logic code
-For use on Amazon's Lambda AWS service with Alexa Skill "RPG Helper" only
+For use on Amazon's Lambda AWS service with Alexa Skill "Game Helper" only
 
 Developed by Zac Patel on 1/10/17
 
@@ -13,64 +13,80 @@ from __future__ import print_function
 from random import randint
 
 from SpeechHelpers import build_speechlet_response, build_response
-from RiskLogic import RiskBattleHandler, RiskProbabilityHandler
+from WargameLogic import battle_handler, battle_probability_handler
+#from WordHelper import word_value_handler, word_checker_handler, word_spell_handler
+
 # --------------- Functions that control the skill's behavior ------------------
 # Note: I specify a default value for numDice, but Alexa's voice processing shouldEndSession
 # always give us a value here (since it turns 'a' to 1)
-def roll_dice(numDice, numSides, modifier=0):
+def roll_dice(num_dice, num_sides, modifier=0):
     # Note: we don't add modifier to the randint range because it is only added once
-    mysum = modifier
-    for _ in range(0, numDice):
-        mysum += randint(0, numSides)
-    return mysum
+    my_sum = modifier
+    for _ in range(0, num_dice):
+        my_sum += randint(0, num_sides)
+    return my_sum
 
 
-def modifier_to_number(modifierType, modifierValue):
+def modifier_to_number(modifier_type, modifier_value):
     # list of possible values that define the "sign" of the modifier
     # note we handle them in here because
     positiveModifierValues = {"adding, add, plus"}
     negativeModifierValues = {"subtract", "sub", "minus"}
-    if modifierType in positiveModifierValues:
-        return modifierValue
-    elif modifierType in negativeModifierValues:
-        return 0 - modifierValue
+    if modifier_type in positiveModifierValues:
+        return modifier_value
+    elif modifier_type in negativeModifierValues:
+        return 0 - modifier_value
     else: # default case kept to clarify that ambiguous answers should be added by default
-        return modifierValue
+        return modifier_value
 
 # Generates a user-readable string from a dice roll result
-def create_result_string(rollValue, profileName="",):
-    if profileName == "":
-        return "Your roll is " + str(rollValue)
+def create_result_string(roll_value, profile_name="",):
+    if profile_name == "":
+        return "Your roll is " + str(roll_value)
     else:
-        return "The roll from " + profileName + " is " + str(rollValue)
+        return "The roll from " + profile_name + " is " + str(roll_value)
 
 # Single function that generates a random number according to specified parameters
 # and returns a built speechlet response
-def roll_dice_intent_handler(intentSlots, profileName=""):
+def roll_dice_intent_handler(intent_slots, profileName=""):
     # Pulling the dice and sides values (which are in all rolling intents)
     # Note, we have to coerce these values into ints because they are stored as strings
-    numDice, numSides = int(intentSlots["numDice"]["value"]), int(intentSlots["numSides"]["value"])
-    rollValue, modifier = 0, 0
+    num_dice, num_sides = int(intent_slots["numDice"]["value"]), int(intent_slots["numSides"]["value"])
+    roll_value, modifier = 0, 0
 
     # Calculating a modifier if one has been specified by the user
-    if "adding" in intentSlots:
-        modifier = modifier_to_number(intentSlots["adding"]["value"], int(intentSlots["modifier"]["value"]))
+    if "adding" in intent_slots:
+        modifier = modifier_to_number(intent_slots["adding"]["value"], int(intent_slots["modifier"]["value"]))
 
     # Calculating the value of the dice roll
-    rollValue = roll_dice(numDice, numSides, modifier)
+    roll_value = roll_dice(num_dice, num_sides, modifier)
 
     session_attributes = {}
 
     # Constructing a response string that describes the dice roll result
-    resultString = create_result_string(rollValue, profileName)
+    result_string = create_result_string(roll_value)
 
     # the title of the card that appears in the alexa phone app
     card_title = "Dice Roll"
 
     # Note: we do not set a reprompt_text because in most use cases, the user
     # will only want to roll one dice at a time (hence, should_end_session is True)
-    return build_response(session_attributes,
-        build_speechlet_response(card_title, resultString, None, True))
+    return build_response(session_attributes, build_speechlet_response(card_title, result_string, None, True))
+
+BAD_NUM_INPUT = "BAD_NUM_INPUT"
+NO_INPUT = "BAD_INPUT"
+
+def get_dice_from_intent(intent):
+    """
+    Handles the error trapping for reading a dice roll off the intent
+    """
+    num_dice, num_sides, modifier = NO_INPUT, NO_INPUT, NO_INPUT
+    
+    try:
+        num_dice = int(intent["numDice"]["value"])
+    except:
+        num_dice = BAD_NUM_INPUT
+
 
 
 # TODO: build a method that goes into user stored data, grabs the dice profile
@@ -80,9 +96,12 @@ def roll_dice_intent_handler(intentSlots, profileName=""):
 
 # Event Handler Methods
 def get_welcome_response():
+    """
+    Called if the user starts a session without specifying an intent
+    """
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Wlecome to RPG Helper. What can I help you with?"
+    speech_output = "I can help with dice and Risk."
 
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
@@ -92,8 +111,11 @@ def get_welcome_response():
         card_title, speech_output, reprompt_text, should_end_session))
 
 def handle_session_end_request():
+    """
+    Called when the user tries to cancel or stop the current session
+    """
     card_title = "Session Ended"
-    speech_output = "Thank you for using RPG Helper. "
+    speech_output = "Thank you for using Game Helper."
 
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
@@ -132,12 +154,20 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Selecting different behavior for different intent types
-    if intent_name == "RollDiceIntent":
+    if intent_name == "ROLLDICEINTENT":
         return roll_dice_intent_handler(intent["slots"])
-    elif intent_name == "RollDiceWithModifierIntent":
-        return roll_dice_intent_handler(intent["slots"])
-    elif intent_name == "SimulateRiskBattleIntent":
-        return RiskBattleHandler(intent["slots"])
+    elif intent_name == "SIMULATEBATTLEINTENT":
+        return battle_handler(intent["slots"])
+    elif intent_name == "CALCULATEBATTLEPROBABILITYINTENT":
+        return battle_probability_handler(intent["slots"])
+    #elif intent_name == "GETWORDVALUEINTENT":
+    #    return None
+    #elif intent_name == "SPELLWORDINTENT":
+    #    return None
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
     else:
         raise ValueError("Invalid intent")
 
--- RiskLogic.py
+++ RiskLogic.py
@@ -10,149 +10,166 @@ from random import randint
 from SpeechHelpers import build_speechlet_response, build_response
 
 # --------------- Complete behavior functions that can be called by other files ---------------
-def RiskBattleHandler(intent):
+def battle_handler(intent):
     # Taking advantage of python's ability to return multiple items
     # Reads the number of attackers and defenders from the intent
-    numAttackers, numDefenders = getNumAttDef(intent)
+    num_attackers, num_defenders = get_num_att_def(intent)
 
-    # simulates a risk battle to get the final number of attackers and defenders
-    finalAttackers, finalDefenders, numRounds = simulateRiskBattle(numAttackers, numDefenders)
+    # simulates a battle to get the final number of attackers and defenders
+    final_attackers, final_defenders, num_rounds = simulate_battle(num_attackers, num_defenders)
 
     # generates a battle summary string from the results
-    battleResultString = createRiskBattleResultString(numAttackers, numDefenders, finalAttackers, finalDefenders)
+    battle_res_string = create_battle_res_string(num_attackers, num_defenders, final_attackers, final_defenders)
 
     # setting the title of the card that should appear on the phone app
-    card_title = "Risk Battle Simulated"
+    card_title = "Battle Simulated"
 
     session_attributes = {}
     # constructs and returns a completed Alexa response
     # note: no reprompt text is given because in the course of a game, a battle would only be run once
-    return build_response(session_attributes,
-        build_speechlet_response(card_title, battleResultString, None, True))
+    return build_response(session_attributes, build_speechlet_response(card_title, battle_res_string, None, True))
 
-def RiskProbabilityHandler(intent):
+def battle_probability_handler(intent):
     # read the number of attackers and defenders from the intent
-    numAttackers, numDefenders = getNumAttDef(intent)
+    num_attackers, num_defenders = get_num_att_def(intent)
 
     # search the probability table for the corresponding probability
-    prob =  findRiskBattleProbability(numAttackers, numDefenders)
+    prob = find_battle_probability(num_attackers, num_defenders)
 
     # construct a statement about the probability
-    probResponseString = createProbabilityResponseString(prob)
+    prob_res_str = create_prob_res_str(prob)
 
     # setting the title of the card that should appear on the phone app
-    card_title = "Risk Probability Calculated"
+    card_title = "Battle Probability Calculated"
 
     session_attributes = {}
     # construct a reply to the user
-    return build_response(session_attributes,
-        build_speechlet_response(card_title, probResponseString, None, True))
+    return build_response(session_attributes, build_speechlet_response(card_title, prob_res_str, None, True))
 
 # giving several possible phrases for variety (note, these are only given if the user asks for them)
-posRecPhrases = {"I suggest you attack.", "The odds are in favor of attacking.", "You are likely to win."}
-midRecPhrases = {"The fight could go either way.", "There is no obvious winner", "You could try your luck."}
-negRecPhrases = {"I suggest you don't attack", "The odds are against attacking.", "You are not likely to win."}
+pos_rec_phrases = ["I suggest you attack.", "The odds are in favor of attacking.", "You are likely to win."]
+mid_rec_phrases = ["The fight could go either way.", "There is no obvious winner", "You could try your luck."]
+neg_rec_phrases = ["I suggest you don't attack", "The odds are against attacking.", "You are not likely to win."]
 
-# creates a string describing the probability about a battle
-def createProbabilityResponseString(prob, recommendation=False):
-    probString = ""
+def create_prob_res_str(prob, recommendation=False):
+    """
+    returns a string describing the probability of a battle the user created
+    """
+    prob_string = ""
     if prob == -1:
-        probString = "This function can only predict battles between 10 or fewer units on each side. In general, attackers have advantage at higher army numbers."
+        prob_string = "This function can only predict battles between 10 or fewer units on each side. In general, attackers have advantage at higher army numbers."
     else:
-        probString = "Attackers have a " + str(prob * 100) + " percent chance of winning the battle."
+        prob_string = "Attackers have a " + str(prob * 100) + " percent chance of winning the battle."
 
     # add a recommendation if the user wants one
     if recommendation:
         if prob > .6:
-            probString = probString + posRecPhrases[randint(0, len(posRecPhrases))]
+            prob_string = prob_string + pos_rec_phrases[randint(0, len(pos_rec_phrases))]
         elif .4 <= prob <= .6:
-            probString = probString + midRecPhrases[randint(0, len(midRecPhrases))]
+            prob_string = prob_string + mid_rec_phrases[randint(0, len(mid_rec_phrases))]
         else:
-            probString = probString + negRecPhrases[randint(0, len(negRecPhrases))]
+            prob_string = prob_string + neg_rec_phrases[randint(0, len(neg_rec_phrases))]
 
-    return probString
+    return prob_string
 # ---------------Helper functions that should not be used outside of this file---------------
 # (generally listed in the order in which they would be called)
 
-# Reads the number of attackers and defenders from an intent, and returns them in an ordered pair
-def getNumAttDef(intent):
+
+def get_num_att_def(intent):
+    """
+    Reads the number of attackers and defenders from an intent, and returns them in an ordered pair"
+    """
     # Determines in which order the user listed attackers and defenders
-    numPartyOne, numPartyTwo = int(intent["numPartyOne"]["value"]), int(intent["numPartyTwo"]["value"])
+    num_party_one, num_party_two = int(intent["numPartyOne"]["value"]), int(intent["numPartyTwo"]["value"])
     party1Type, party2Type = int(intent["partyOneType"]["value"]), int(intent["partyTwoType"]["value"])
 
     # Attacker / Defender Keywords (to match custom type)
-    attackerKeywords = {"attacking", "attackers"}
-    defenderKeywords = {"defender", "defending"}
+    attacker_keywords = {"attacking", "attackers"}
+    defender_keywords = {"defender", "defending"}
 
     # default behavior for this statement is attackers first, then defenders second
-    if (party1Type in defenderKeywords):
-        return numPartyTwo, numPartyOne
+    if (party1Type in defender_keywords):
+        return num_party_two, num_party_one
     else:
-        return numPartyOne, numPartyTwo
+        return num_party_one, num_party_two
 
-def simulateRiskBattle(numAttackers, numDefenders):
+def simulate_battle(num_attackers, num_defenders):
+    """
+    simulates the entirety of a battle until the one side is defeated
+    returns the number of remaining attackers, defenders and turns taken
+    """
     # Tracker for the number of rounds needed to do the battle (just for funsies)
-    numRounds = 0
+    num_rounds = 0
 
     # note, we include the attacker that is "left behind" in our calculations to better represent
     # how calculations would be done by hand
     # Each iteration of the while loop represents an individual "battle" / "dice roll"
-    while numAttackers > 1 and numDefenders > 0:
-        numRounds += 1
+    while num_attackers > 1 and num_defenders > 0:
+        num_rounds += 1
 
-        attackerRolls, defenderRolls = [], []
+        attacker_rolls, defender_rolls = [], []
 
         # including checks for the special cases where attackers / defenders have less than full strength
-        if numAttackers > 3:
-            attackerRolls = generateRiskBattleRolls(3)
+        if num_attackers > 3:
+            attacker_rolls = generate_battle_rolls(3)
         else:
-            attackerRolls = generateRiskBattleRolls(numAttackers - 1)
+            attacker_rolls = generate_battle_rolls(num_attackers - 1)
 
-        if numDefenders > 2:
-            defenderRolls = generateRiskBattleRolls(2)
+        if num_defenders > 2:
+            defender_rolls = generate_battle_rolls(2)
         else:
-            defenderRolls = generateRiskBattleRolls(numDefenders)
+            defender_rolls = generate_battle_rolls(num_defenders)
 
 
         # sorting the rolls so we can compare the two highest rolls
-        attackerRolls.sort()
-        defenderRolls.sort()
+        attacker_rolls.sort()
+        defender_rolls.sort()
 
         # number of wins is measures from the attackers perspective
         # a "fight" in this instance is the comparison between two dice rolls
-        wins, numFights = 0, min(numAttackers -1, numDefenders)
-        for i in range(0, numFights):
-            if attackerRolls[i] > defenderRolls[i]:
+        wins, num_fights = 0, min(num_attackers -1, num_defenders)
+        for i in range(0, num_fights):
+            if attacker_rolls[i] > defender_rolls[i]:
                 wins += 1
 
         # recalculating the number of attackers and defenders after the fight
-        numAttackers, numDefenders = numAttackers - (numFights - wins), numDefenders - wins
+        num_attackers, num_defenders = num_attackers - (num_fights - wins), num_defenders - wins
 
-    return numAttackers, numDefenders, numRounds
+    return num_attackers, num_defenders, num_rounds
 
-# Returns an array representing all the dice rolls by one side in a battle
-def generateRiskBattleRolls(numRolls):
+def generate_battle_rolls(numRolls):
+    """
+    Returns an array representing all the dice rolls by one side in a battle
+    """
     return [randint(1, 6) for _ in range(0, numRolls)]
 
-# creates a string summarizing the result of a risk battle
-def createRiskBattleResultString(initAttackers, initDefenders, finalAttackers, finalDefenders):
-    if finalDefenders == 0:
-        return "The attackers won a battle of " + str(initAttackers) + " vs " + str(initDefenders) + " with " + str(finalAttackers) + " remaining."
+def create_battle_res_string(init_attackers, init_defenders, final_attackers, final_defenders):
+    """
+    reates a string summarizing the result of a battle
+    """
+    if final_defenders == 0:
+        return "The attackers won a battle of " + str(init_attackers) + " vs " + str(init_defenders) + " with " + str(final_attackers) + " remaining."
     else:
-        return "The defenders survived a battle of " + str(initAttackers) + " vs " + str(initDefenders) + " with " + str(finalDefenders)+ "remaining."
+        return "The defenders survived a battle of " + str(init_attackers) + " vs " + str(init_defenders) + " with " + str(final_defenders)+ "remaining."
 
-# returns -1 if the number of armies is outside the bounds of the chart
-# may be adapted later to include full probability calculations
-# note this method adjusts for the attacker that must be left behind
-def findRiskBattleProbability(numAttackers, numDefenders):
-    if numAttackers > 12 or numAttackers <= 1 or  numDefenders > 11 or numDefenders < 0:
+
+def find_battle_probability(num_attackers, num_defenders):
+    """
+    Takes in a number of attackers and defenders (from 1-10)
+    Returns the probability that the defenders will be defeated before attackers are forced to stop
+    returns -1 if the number of armies is outside the bounds of the chart
+
+    (may be adapted later to include full probability calculations)
+    note this method adjusts for the attacker that must be left behind
+    """
+    if num_attackers > 12 or num_attackers <= 1 or  num_defenders > 11 or num_defenders < 0:
         return -1
     else:
-        return riskProbabilityChart[numAttackers - 2][numDefenders - 1]
+        return prob_chart[num_attackers - 2][num_defenders - 1]
 
 # Taken from Jason A. Osborne's paper: http://www4.stat.ncsu.edu/~jaosborn/research/RISK.pdf
 # x -> attackers, y -> defenders
-riskProbabilityChart = [
+prob_chart = [
 [0.417, 0.106, 0.027, 0.007, 0.002, 0.000, 0.000, 0.000, 0.000, 0.000],
 [0.754, 0.363, 0.206, 0.091, 0.049, 0.021, 0.011, 0.005, 0.003, 0.001],
 [0.916, 0.656, 0.470, 0.315, 0.206, 0.134, 0.084, 0.054, 0.033, 0.021],
