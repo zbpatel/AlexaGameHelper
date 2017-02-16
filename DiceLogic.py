3ae0401c97700b49c321fe33107a73472d5abd00
Zac Patel <zpatel@berkeley.edu>
2017-02-15 19:07:48 -0800

    finished initial reworking, uploading to AWS for testing.

--- DiceLogic.py
+++ DiceLogic.py
@@ -12,7 +12,7 @@ from SpeechHelpers import build_response, build_speechlet_response
 # defining a set of variables that we can use to internally specify our error types
 BAD_ROLL_INPUT = "BAD_ROLL_INPUT"
 INPUT_NOT_NUMBER = "INPUT_NOT_NUMBER"
-NO_INPUT = "BAD_INPUT"
+NO_INPUT = "NO_INPUT"
 
 # Single function that generates a random number according to specified parameters
 # and returns a built speechlet response
@@ -24,17 +24,23 @@ def roll_dice_handler(intent):
     # switching the intent over to the slots so we can pull our input values out
     intent = intent["slots"]
 
-    # Pulling the dice and sides values (which are in all rolling intents)
-    # Note, we have to coerce these values into ints because they are stored as strings
-    num_dice, num_sides = int(intent["numDice"]["value"]), int(intent["numSides"]["value"])
-    roll_value, modifier = 0, 0
-
-    # Calculating a modifier if one has been specified by the user
-    if "adding" in intent:
-        modifier = modifier_to_number(intent["adding"]["value"], int(intent["modifier"]["value"]))
-
-    # Calculating the value of the dice roll
+    # grabbing the dice from the intent, processing them, then saving them to the respective vars
+    num_dice, num_sides, modifier = get_dice_from_intent(intent)
+    
+    # Calculating the value of the dice roll via roll_dice
+    # NOTE: we don't do any special error trapping here, because if any of the values is not an int
+    # I.E. an error message, roll_dice will exit before it errors
     roll_value = roll_dice(num_dice, num_sides, modifier)
+
+    if roll_value == BAD_ROLL_INPUT:
+        card_title = "Coudldn't Roll Dice"
+        res_str = "I'm sorry, I could not understand your request."
+    else:      
+        # the title of the card that appears in the alexa phone app
+        card_title = "Dice Rolled"
+
+        # Constructing a response string that describes the dice roll result
+        res_str = create_result_string(roll_value)
 
     # no session attributes necessary (maybe this will be changed later)
     session_attributes = {}
@@ -42,12 +48,6 @@ def roll_dice_handler(intent):
     # does not prompt the user for a response after making their call
     # (this might be changed later to allow the user to easily repeat a single roll_dice)
     should_end_session = True
-
-    # Constructing a response string that describes the dice roll result
-    res_str = create_result_string(roll_value)
-
-    # the title of the card that appears in the alexa phone app
-    card_title = "Dice Roll"
 
     # Note: we do not set a reprompt_text because in most use cases, the user
     # will only want to roll one dice at a time (hence, should_end_session is True)
@@ -80,14 +80,17 @@ def modifier_to_number(modifier_type, modifier_value):
     If no modifier type is given, the program will assume the user wants to add.
     """
     # here we define a set of possible words the user could input to specify addding / subtracting
-    pos_mod_vals = ["adding", "add", "plus"]
+    # NOTE: we don't actually use pos_mod_values because the default behavior is to return a
+    # positive modifier value (I might change this later, its mildly goony)
+
+    #pos_mod_vals = ["adding", "add", "plus"]
     neg_mod_vals = ["subtract", "sub", "minus"]
 
-    if modifier_type in pos_mod_vals:
+    if modifier_type == NO_INPUT:
         return modifier_value
     elif modifier_type in neg_mod_vals:
         return 0 - modifier_value
-    else: # default case kept to clarify that ambiguous answers should be added by default
+    else:
         return modifier_value
 
 # Generates a user-readable string from a dice roll result
@@ -95,7 +98,7 @@ def create_result_string(roll_value):
     """
     Returns a basic string about a roll of roll_value that Alexa will read to the user.
     """
-    return "Your roll is " + str(roll_value)
+    return "Your roll is " + str(roll_value) + "."
 
 
 def get_dice_from_intent(intent):
@@ -107,14 +110,20 @@ def get_dice_from_intent(intent):
 
     if "numDice" in intent:
         num_dice = process_num(intent["numDice"]["value"])
-    
+
     if "numSides" in intent:
         num_sides = process_num(intent["numSides"]["value"])
-    
+
     # this is really the only case that needs checking on a proper call
     if "modifier" in intent:
         modifier = process_num(intent["modifier"]["value"])
-    
+
+        mod_type = NO_INPUT
+        # pulling the modifier out of the intent
+        if "adding" in intent:
+            mod_type = intent["adding"]["value"]
+        modifier = modifier_to_number(mod_type, modifier)
+
     return num_dice, num_sides, modifier
 
 # method to convert an intent value into an integer, and trap errors
